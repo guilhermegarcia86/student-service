56afb16db75c2a91f00e98a0af7bfe8c42e41406
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-10-18 20:48:02 -0300

    first commit

---
+++ src/main/java/br/com/guilherme/garcia/studentservice/StudentServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.guilherme.garcia.studentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApplication.class, args);
+	}
+}
